877aa88b01594884b2964974f33370a2a5bbe5a6
Andrea103 <andreasierra103@gmail.com>
2023-05-30 23:54:14 +0200

    primera letra mayuscula

--- carro.js
+++ carro.js
@@ -1,11 +1,11 @@
 let car = {
-    marca:"Mercedes";
-    puertas:"3";
-    atributos:["Rapido","Confortable","deportivo"];
-    console.log (car);
-    console.log (car.marca);
-    console.log (car.puertas);
-    console.log (car.atributos[1]);
+    marca:"Mercedes",
+    puertas:"3",
+    atributos:["Rapido","Confortable","deportivo"],
+    console.log (car),
+    console.log (car.marca),
+    console.log (car.puertas),
+    console.log (car.atributos[1]),
 
 }
 car.atributos[1] = "hermoso";
